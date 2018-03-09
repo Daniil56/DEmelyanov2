ec34020cb18a533d7a2046b2d01e7fcda86cb642
Daniil56 <emelyanov.daniil93@gmail.com>
2018-03-09 21:47:50 +0500

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,14 +1,26 @@
-/**
-* Package for calculate task.
-*
-* @author Daniil Emelianov
-* @version 0.1
-* @since 09/03/2018
-*/
 package ru.job4j;
 
+/**
+* Calculate.
+*
+* @author Daniil Emelianov
+* @version $id$
+* @since 09/03/2018
+*/
 public class Calculate {
-public static void main (String[] args) {
+	/**
+	* Main.
+	* @param args - args.
+	*/
+public static void main(String[] args) {
 		System.out.println ("Hello World");
-}
+	}
+	/**
+	* Method echo.
+	* @param name your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+	}
 }
