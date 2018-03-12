05e71128ccf42051f02579322bf34eda022a24cf
Daniil56 <emelyanov.daniil93@gmail.com>
2018-03-12 20:41:33 +0500

    3.3. Идеальный вес. [#41584] константы

--- chapter_001/src/main/java/ru/job4j/calculator/Fit.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -4,18 +4,22 @@ package ru.job4j.calculator;
  * Программа расчета идеального веса.
  */
 public class Fit {
-    private double weight = 0;
+    private double weight;
+    private final double idealHeightMan = 100;
+    private final double idealHeightWooman = 110;
+    private final double growthFactor = 1.15;
     /**
      * Идеальный вес ля мужчины.
      * @param height рост.
      * @return идеальный вес.
      */
+
     double manWeight(double height) {
-        this.weight = (height - 100) * 1.15;
-        return  weight;
+        return  this.weight = (height - idealHeightMan) * growthFactor;
+
     }
     double womanWeight(double height) {
-        this.weight = (height - 110) * 1.15;
-        return  weight;
+       return this.weight = (height - idealHeightWooman) * growthFactor;
+
     }
 }
