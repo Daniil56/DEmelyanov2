5d9de16a5722a6441587f011120469bdcb7887b4
Daniil56 <emelyanov.daniil93@gmail.com>
2018-03-12 23:55:52 +0500

    4.4. Максимум из трех чисел [#190]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -5,14 +5,27 @@ package ru.job4j.max;
  * @version $id$
  * @since  12/03/2018
  */
-public class Max {
+public  class Max {
     /**
-     * Отвечает на вопросы.
-     * @param first первое значение
+     * Метод возвращающий максимум их двух чисел.
+     *
+     * @param first  первое значение
      * @param second второе значение
      * @return наибольшое значение.
      */
     public int max(int first, int second) {
-        return  first > second ? first : second;
+        return first > second ? first : second;
+    }
+    /**
+     * Метод возвращающий максимум их трех чисел.
+     *
+     * @param first  первое значение
+     * @param second второе значение
+     * @param third третье значене
+     * @return наибольшое значение.
+     */
+    public int max(int first, int second, int third) {
+        int temp = this.max(first, second);
+        return  max((temp), third);
     }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -4,12 +4,19 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+
+
 public class MaxTest {
     @Test
     public void whenFirstLessSecond() {
         Max maximum = new Max();
-        int result = maximum.max(1, 2);
-        assertThat(result, is(2));
-
+        int result = maximum.max(8, 2);
+        assertThat(result, is(8));
     }
-}
+    @Test
+    public void whenFirstLessSecondLessThird() {
+        Max maximum3 = new Max();
+        int result = maximum3.max(28, 16, 15);
+        assertThat(result, is(28));
+    }
+    }
