936b19231b4313333459b5cea1996c0cc5720ff3
Daniil56 <emelyanov.daniil93@gmail.com>
2018-04-17 23:28:28 +0500

    Каркас шахматной доски [#792]  пред.

--- chapter_002/src/main/java/ru/job4j/chess/Bishop.java
+++ chapter_002/src/main/java/ru/job4j/chess/Bishop.java
@@ -9,11 +9,23 @@ public class Bishop extends Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-
-        throw new ImposableMoveException("Move not be applied");
+        int count = 1;
+        Cell[] currentCourse = new Cell[0];
+        if (Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY()))
+            if ((source.getX() - dest.getX()) <= -1 && (source.getY() - dest.getY()) <= -1) {
+                for (int i = source.getX() + 1, j = source.getY() + 1; count <= Math.abs(dest.getX() - source.getX()); i++, j++) {
+                    currentCourse[count - 1] = new Cell(i, j);
+                    count++;
+                }
+                throw new ImposableMoveException("Move not be applied");
+            }
+        return currentCourse;
     }
 
+    @Override
     public Figure copy(Cell dest) {
         return new Bishop(dest);
     }
+
+
 }
--- chapter_002/src/main/java/ru/job4j/chess/Figure.java
+++ chapter_002/src/main/java/ru/job4j/chess/Figure.java
@@ -3,11 +3,14 @@ package ru.job4j.chess;
 public abstract class Figure {
    private final Cell position  ;
 
-    public    Figure(Cell position) {
+    public  Figure(Cell position) {
         this.position = position;
     }
 
     public  abstract Cell[] way(Cell source, Cell dest)
+            // dest - задает ячейку, куда следует пойти. Если фигура может туда пойти. то Вернуть массив ячеек. которые должна пройти фигура.
+        //
+        //    Если фигура туда пойти не может. выбросить исключение ImposibleMoveException
             throws  ImposableMoveException;
 
     public abstract Figure copy(Cell dest);
