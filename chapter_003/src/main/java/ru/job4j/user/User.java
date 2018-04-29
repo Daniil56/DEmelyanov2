b7481f1e3a95f4c5b5a52fb1c0ed191689b19ee8
Daniil56 <emelyanov.daniil93@gmail.com>
2018-04-29 20:55:55 +0500

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/user/User.java
+++ chapter_003/src/main/java/ru/job4j/user/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.user;
 
 
+import java.util.Objects;
+
 public class User implements Comparable<User> {
     private int id;
     private String name;
@@ -35,13 +37,28 @@ public class User implements Comparable<User> {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, age);
+    }
+
+    @Override
     public String toString() {
-        return "User{" + "id=" + id + ", name='" + name + '\'' + ", city='" + city + '\'' + '}';
+        return String.format("%s, %d", name, age);
     }
 
     @Override
     public int compareTo(User o) {
       int dif = (this.age - o.age);
-        return dif == 0? 0: dif > 0 ? 1: -1;
+        return Integer.compare(dif, 0);
     }
 }
--- chapter_003/src/test/java/ru/job4j/list/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/list/SortUserTest.java
@@ -30,5 +30,54 @@ public class SortUserTest {
         actual = user.sort(users);
         assertThat(actual, is(expectSet));
     }
+    @Test
+    public void whenListThenSortByLength() {
+        SortUser user = new SortUser();
+        List<User> users = new ArrayList<>();
+        List<User> actual;
+        User daniil = new User("Daniil", 24);
+        User ivan = new User("Ivan", 30);
+        User anton = new User("Anton", 20);
+        User ban = new User("Ban", 18);
+        users.add(daniil);
+        users.add(ivan);
+        users.add(anton);
+        users.add(ban);
+        List<User> expectList = new ArrayList<>();
+        expectList.add(ban);
+        expectList.add(ivan);
+        expectList.add(anton);
+        expectList.add(daniil);
+        actual = user.sortNameLength(users);
+        assertThat(actual, is(expectList));
+    }
+
+    @Test
+    public void whenListThenSortByAllFields() {
+        SortUser user = new SortUser();
+        List<User> users = new ArrayList<>();
+        List<User> actual;
+        User daniil = new User("Daniil", 24);
+        User ivan = new User("Ivan", 18);
+        User ivan23 = new User("Ivan", 23);
+        User anton = new User("Anton", 20);
+        User ban = new User("Ban", 18);
+        User sergei = new User("Sergei", 21);
+        users.add(daniil);
+        users.add(ivan);
+        users.add(ivan23);
+        users.add(anton);
+        users.add(sergei);
+        users.add(ban);
+        List<User> expectList = new ArrayList<>();
+        expectList.add(anton);
+        expectList.add(ban);
+        expectList.add(daniil);
+        expectList.add(ivan);
+        expectList.add(ivan23);
+        expectList.add(sergei);
+        actual = user.sortByAllFields(users);
+        assertThat(actual, is(expectList));
+    }
 
 }
