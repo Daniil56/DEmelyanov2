01988b533707389a59a1fc9a62a8e1ef8419d862
Daniil56 <emelyanov.daniil93@gmail.com>
2018-06-07 17:43:29 +0500

    5.2.2. Реализовать Store<T extends Base> [#][#56299]

--- collection_pro/src/main/java/ru/job4j/generic/UserStore.java
+++ collection_pro/src/main/java/ru/job4j/generic/UserStore.java
@@ -1,44 +1,31 @@
 package ru.job4j.generic;
 
-public class UserStore implements Store<User> {
+public class UserStore extends AbstractStore<User> {
     private SimpleArray<User> value;
 
-    public UserStore(int size) {
-        this.value = new SimpleArray<>(size);
-    }
 
+    protected UserStore(int size) {
+        super(size);
+    }
 
     @Override
     public void add(User model) {
-        value.add(model);
+        super.add(model);
     }
 
     @Override
     public boolean replace(String id) {
-        boolean result = false;
-      if (findVyId(id) != null) {
-          result = true;
-      }
-        return result;
+        return super.replace(id);
     }
 
     @Override
     public boolean delete(String id) {
-        boolean result = false;
-        if (findVyId(id) != null) {
-            result = true;
-        }
-            return result;
+        return super.delete(id);
     }
 
     @Override
     public User findVyId(String id) {
-        User itUser = null;
-     for (int index = 0; index < value.size(); index++) {
-         if (id.equals(value.get(index).getId())) {
-             itUser = value.get(index);
-         }
-     }
-        return itUser;
+        return super.findVyId(id);
     }
+
 }
