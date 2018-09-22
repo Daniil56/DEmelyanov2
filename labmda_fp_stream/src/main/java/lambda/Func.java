0d1b6c8787908b5ecb8865eb9c6d5b05823d6c70
Daniil56 <emelyanov.daniil93@gmail.com>
2018-09-22 23:09:11 +0500

    1. Подсчет функции в диапазоне. [#24251]

---
+++ labmda_fp_stream/src/main/java/lambda/Func.java
@@ -0,0 +1,16 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Func {
+
+    public List<Double> diapason(int start, int end, Function<Double, Double> function) {
+        List<Double> result = new ArrayList<>();
+        for (int index = start; index <= end; index++) {
+            result.add(function.apply((double) index));
+        }
+        return result;
+    }
+}
