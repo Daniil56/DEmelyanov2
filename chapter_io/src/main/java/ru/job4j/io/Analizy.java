e6580db1a9d3d091c55f3226e62ec871f01d50fa
Daniil <emelyanov.daniil93@gmail.com>
2020-03-02 12:39:43 +0500

    task #173905 test io - complete

--- chapter_io/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_io/src/main/java/ru/job4j/io/Analizy.java
@@ -10,13 +10,15 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class Analizy {
     private final String pathIn;
     private final String pathOut;
-    final AtomicBoolean server = new AtomicBoolean(true);
+    private final List<String> unavailableList = new ArrayList<>();
+    final AtomicBoolean server  = new AtomicBoolean(true);
 
 
     public Analizy(String in, String out) {
         this.pathIn = in;
         this.pathOut = out;
     }
+
 
     private static String[][] apply(String[] splitTarget) {
         String[] split = splitTarget[0].split(";");
@@ -25,60 +27,61 @@ public class Analizy {
     }
 
 
-    public void init(Map<String, String> data) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(pathIn))) {
+    public void init(Map<String, String> data, PrintWriter out) {
             data.forEach((key, value) ->
                     out.printf("%s %s%n", key, value));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
     public List<String> unavailable() {
-        final List<String> unavailableList = new ArrayList<>();
+        List<String> result = new ArrayList<>();
         try (BufferedReader readerIn = new BufferedReader(new FileReader(this.pathIn));
              PrintWriter out = new PrintWriter(new FileOutputStream(this.pathOut))) {
-            final String[] join = {""};
-            readerIn.lines()
-                    .filter(Objects::nonNull)
-                    .filter(val -> !val.isEmpty())
-                    .map(str -> str.split(" "))
-                    .forEach(logg -> {
-                        int status = Integer.parseInt(logg[1]);
-                        String time = logg[0];
-                        if (server.get() && status == 500 || status == 400) {
-                            out.printf("%s;", time);
-                            join[0] = join[0].concat(time + ";");
-                            server.set(false);
-                        }
-                        if (!server.get() && status == 200 || status == 300) {
-                            out.printf("%s;\n", time);
-                            join[0] = join[0].concat(time + ";\n");
-                            server.set(true);
-                        }
-                    });
-            unavailableList.add(join[0]);
+            addUnavailableFromList(readerIn, out);
+            result = sourceWrite(unavailableList, out);
         } catch (IOException e) {
             e.printStackTrace();
         }
-       return sourceWrite(unavailableList);
+       return result;
     }
 
-    private List<String> sourceWrite(List<String> unavailableList) {
+    public void addUnavailableFromList(BufferedReader readerIn, PrintWriter out) {
+        final String[] join = {""};
+        readerIn.lines()
+                .filter(Objects::nonNull)
+                .filter(val -> !val.isEmpty())
+                .map(str -> str.split(" "))
+                .forEach(logg -> {
+                    int status = Integer.parseInt(logg[1]);
+                    String time = logg[0];
+                    if (server.get() && status == 500 || status == 400) {
+                        out.printf("%s;", time);
+                        join[0] = join[0].concat(time + ";");
+                        server.set(false);
+                    }
+                    if (!server.get() && status == 200 || status == 300) {
+                        out.printf("%s;\n", time);
+                        join[0] = join[0].concat(time + ";\n");
+                        server.set(true);
+                    }
+                });
+        unavailableList.add(join[0]);
+    }
+
+    public List<String> sourceWrite(List<String> unavailableList, PrintWriter inWrite) {
         List<String> result = new ArrayList<>();
-        try (PrintWriter inWrite = new PrintWriter(new FileOutputStream(this.pathOut))) {
-            unavailableList.stream().map(str -> str.split(";\n")).map(Analizy::apply).forEach(period -> {
-                result.add(period[0][0]);
-                result.add(period[0][period[0].length - 1]);
-                inWrite.printf("%s;%s;%n", period[0][0], period[0][period[0].length - 1]);
-                result.add(period[period.length - 1][0]);
-                result.add(period[period.length - 1][period[period.length - 1].length - 1]);
-                inWrite.printf("%s;%s;%n", period[period.length - 1][0], period[1][period[period.length - 1].length - 1]);
-            });
-            unavailableList.clear();
-        } catch (IOException exc) {
-            exc.printStackTrace();
-        }
+        unavailableList.stream().map(str -> str.split(";\n")).map(Analizy::apply).forEach(period -> {
+            result.add(period[0][0]);
+            result.add(period[0][period[0].length - 1]);
+            inWrite.printf("%s;%s;%n", period[0][0], period[0][period[0].length - 1]);
+            result.add(period[period.length - 1][0]);
+            result.add(period[period.length - 1][period[period.length - 1].length - 1]);
+            inWrite.printf("%s;%s;%n", period[period.length - 1][0], period[1][period[period.length - 1].length - 1]);
+        });
+        unavailableList.clear();
         return result;
+    }
+
+    public List<String> getUnavailableList() {
+        return unavailableList;
     }
 }
--- chapter_io/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_io/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,36 +1,85 @@
 package ru.job4j.io;
 
+import org.jetbrains.annotations.NotNull;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+
     @Test
-    public void whenUnavailableRun()  {
+    public void whenUnavailableIntegrateRun() throws FileNotFoundException {
         String in = "src/main/resources/source.csv";
         String out = "src/main/resources/target.csv";
         Analizy analizy = new Analizy(in, out);
-        Map<String, String> data = new LinkedHashMap<>();
+        Map<String, String> data = getDataMap();
+        analizy.init(data, new PrintWriter(out));
+        List<String> expect = getExpectedList();
+        assertThat(analizy.unavailable(), is(expect));
+    }
+
+    @NotNull
+    public List<String> getExpectedList() {
+        return List.of(
+                "10:57:01",
+                "10:59:01",
+                "11:01:02",
+                "11:02:02");
+    }
+
+    @NotNull
+    public Map<String, String> getDataMap() {
+        Map<String, String>  data =  new LinkedHashMap<>();
         data.put("10:56:01", "200");
         data.put("10:57:01", "500");
         data.put("10:58:01", "400");
         data.put("10:59:01", "200");
         data.put("11:01:02", "500");
         data.put("11:02:02", "200");
-        analizy.init(data);
+        return data;
+    }
 
-        List<String> expect = new ArrayList<>();
-        expect.add("10:57:01");
-        expect.add("10:59:01");
-        expect.add("11:01:02");
-        expect.add("11:02:02");
-        assertThat(analizy.unavailable(), is(expect));
+    @Test
+    public void whenAddEdDataListToAnalizyInitThenFileSourceWellBeFill() throws IOException {
+        Analizy analizy = new Analizy(null, null);
+        File source = folder.newFile("source.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            analizy.init(getDataMap(), out);
+        }
+        List<String> initList = new ArrayList<>();
+        List<String> acList = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            reader.lines().forEach(initList::add);
+        }
+        getDataMap().forEach((k, v) -> acList.add(String.format("%s %s", k, v)));
+        assertThat(initList, is(acList));
+    }
+
+    @Test
+    public void whenUnavaibleMockRunThenCreateInTmp() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        Analizy analizy = new Analizy(null, null);
+        try (PrintWriter out = new PrintWriter(source)) {
+            analizy.init(getDataMap(), out);
+        }
+        try (PrintWriter in = new PrintWriter(target);
+             BufferedReader reader = new BufferedReader(new FileReader(source))
+             ) {
+            analizy.addUnavailableFromList(reader, in);
+        }
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            assertThat(analizy.sourceWrite(analizy.getUnavailableList(), out), is(getExpectedList()));
+        }
     }
 }
