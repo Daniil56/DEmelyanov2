ab0c0a9df532067c16f212dd36502edaf7fc2c74
Daniil <emelyanov.daniil93@gmail.com>
2020-03-11 16:22:50 +0500

    task [#106929] - Scan io, complete.

---
+++ chapter_io/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+
+import org.w3c.dom.Node;
+
+import java.io.File;
+import java.util.*;
+import java.util.function.Predicate;
+
+public class Search {
+    List<File> files(String parent, Predicate<String> condition) {
+        List<File> result = new ArrayList<>();
+        File root = new File(parent);
+        Queue<Tree<File>> queue = new ArrayDeque<>();
+        Queue<File> checked = new LinkedList<>();
+        if (root.isDirectory()) {
+            File[] files = root.listFiles();
+            Tree<File> node = new Tree<>(root);
+            node.addChildren(files);
+            queue.add(node);
+          while (!queue.isEmpty()) {
+              var currentNode = queue.remove();
+              var name = currentNode.getValue().getName();
+              if (condition.test(name) && !checked.contains(currentNode.getValue())) {
+                  result.add(currentNode.getValue());
+                  checked.add(currentNode.getValue());
+              } else if (currentNode.getValue().isDirectory()) {
+                  var childes = currentNode.getValue().listFiles();
+                  currentNode.addChildren(childes);
+                  queue.addAll(currentNode.getChildren());
+              }
+          }
+        }
+        return result;
+    }
+}
---
+++ chapter_io/src/test/java/ru/job4j/io/SearchTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SearchTest {
+    @Rule
+    public TemporaryFolder folder =  new TemporaryFolder();
+
+    @Test
+    public void whenSearchIntoConditionThen() throws IOException {
+        Search first = new Search();
+        Predicate<String> conditional = s -> (s.endsWith(".txt"));
+        File firstTxt = folder.newFile("test.txt");
+        File secondTmp = folder.newFile();
+        File nextTmp = folder.newFile();
+        File thirdTxt = folder.newFile("third.txt");
+        folder.newFolder("three");
+        File one = new File(folder.newFolder("one"), "One_Child.txt");
+        one.createNewFile();
+        File twoFolder = folder.newFolder("two");
+        File two = new File(twoFolder, "two.txt");
+        File twoChild = new File(twoFolder, "Two_Child.txt");
+        two.createNewFile();
+        twoChild.createNewFile();
+        List<File> actual = first.files(folder.getRoot().getPath(), conditional);
+        List<File> expected = List.of(firstTxt, thirdTxt, one, two, twoChild);
+        assertThat(actual, is(expected));
+
+
+    }
+
+}
