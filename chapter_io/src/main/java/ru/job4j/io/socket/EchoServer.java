e476c13ae02fcf870754d3f05de993005ede6d6c
Daniil56 <emelyanov.daniil93@gmail.com>
2020-04-14 20:00:07 +0500

    task [#7921] socket Server echo bot completed

--- chapter_io/src/main/java/ru/job4j/io/socket/EchoServer.java
+++ chapter_io/src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
 
@@ -15,17 +16,28 @@ public class EchoServer {
             while (available) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(socket.getInputStream()))) {
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8))) {
                     String message;
-                    while (!(message =  in.readLine()).isEmpty()) {
-                        System.out.println(message);
+                    StringBuilder response = new StringBuilder();
+
+                    while ((message = in.readLine()) != null && !message.isEmpty()) {
                         String[] split = message.split("=");
-                        if (split.length > 1) {
-                            available = !split[1].split(" ")[0].equals("Bye");
+                        if (split.length == 2) {
+                            message = split[1].split(" ")[0];
+                            if ("Exit".equals(message)) {
+                                available = false;
+                                response.append("Goodbye my dear friend").append("\r\n\r\n");
+                            } else {
+                                response.append("Hello dear ").append(message).append("\r\n\r\n");
+                            }
+                            break;
                         }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write(("HTTP/1.1 200 OK\r\n\r\n").getBytes());
+                    out.flush();
+                    out.write(response.toString().getBytes(StandardCharsets.UTF_8));
+                    out.flush();
                 }
             }
         }
